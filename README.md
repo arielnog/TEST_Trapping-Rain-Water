5cb6e2103b6ff383f0cd46537370c8e5cdb146ab
Ariel Nogueira <arielnnogueira@hotmail.com>
2022-08-07 19:59:53 -0300

    Feat: setando index.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# nopaperscanner
+# WaterBox App
+
+## Executando o projeto
+
+###Metodo - Executável (Somente em Windows)
+
+- Clicar no arquivo `waterbox.exe` que a aplicação será executada.
--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-const LogHelper = require('./src/Helpers/LogHelper');
+const LogHelper = require('./src/Helpers/log-helper');
+const Cases = require("./src/Entities/cases");
+const readlineSync = require('readline-sync');
+const CalculateService = require("./src/Service/calculate-service");
+
 
 (async () => {
-    LogHelper.info("[Step - 1] Iniciando o ScannerClient.");
-    readline.question(`What's your name?`, name => {
-        console.log(`Hi ${name}!`);
-        readline.close();
-      });
+    LogHelper.info('Iniciando a aplicação...')
+
+    const repetition = readlineSync.questionInt("Quantas vezes executadas o app? -> ");
+    const cases = new Cases(repetition);
+
+    for (let i = 0; i < cases.getRepetion(); i++) {
+        const width = readlineSync.questionInt("Qual o comprimento da caixa d'agua? -> ");
+
+        if (width <= 2) {
+            LogHelper.error("Comprimento da caixa d'agua inválida.")
+            continue
+        }
+
+        const height = readlineSync.question("Quais as alturas da caixa d'agua? (Insira separadas por spaces) -> ");
+
+        const calculate = await CalculateService.calculate(width, height)
+
+        LogHelper.success("Valores Enviados => " + height + " || Capacidade da caixa d'agua => " + calculate)
+    }
+
+    LogHelper.info('Finalizando a aplicação...')
 })();
 
 
